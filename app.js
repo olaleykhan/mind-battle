8410be9711ad4dc62a9a3d811005714534db6072
lekanDev <realjoy9@gmail.com>
2021-02-26 21:39:58 -0800

    further

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ const app = Vue.createApp({
     data(){
         return{
             creator:"Alaf Consult",
+            monsterHealth: 100,
+            playerHealth:100,
+        }
+    },
+    methods:{
+        attackMonster(){
+            const value= Math.floor(Math.random() * (11 - 5) + 5);
+            this.monsterHealth-=value;
+            setTimeout(()=>{
+                this.attackPlayer();
+            },500)
+            console.log(this.monsterHealth);
+        },
+        attackPlayer(){
+            const value= Math.floor(Math.random() * (11 - 5) + 5);
+            this.playerHealth-=value;
+            
         }
     }
 })
